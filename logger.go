db7e04cc17484d4144cedfebde93b002c43a1f2d
cyrnicolase <nicolase.chen@gmail.com>
2019-08-28 14:59:53 +0800

    增加日志文件模式
    + 增加日志文件分割方式；single, daily
    + Error()方法兼容error

--- logger.go
+++ logger.go
@@ -2,9 +2,21 @@ package logger
 
 import (
 	"os"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
+
+const (
+	// FILE_MODE_SINGLE 单个文件模式
+	FILE_MODE_SINGLE = iota
+
+	// FILE_MODE_DAILY 按天分割
+	FILE_MODE_DAILY
+)
+
+// FileMode 文件模式
+type FileMode int
 
 // FileLogger 文件日志日志
 type FileLogger struct {
@@ -16,14 +28,18 @@ type FileLogger struct {
 
 	// Level 日志记录起始等级
 	Level uint32
+
+	// 文件分割模式
+	Mode FileMode
 }
 
 // NewFileLogger 生成新的日志对象
-func NewFileLogger(filename string, level uint32) (*FileLogger, error) {
+func NewFileLogger(filename string, level uint32, mode FileMode) (*FileLogger, error) {
 	l := &FileLogger{
 		logrus.New(),
 		filename,
 		level,
+		mode,
 	}
 
 	err := l.Init()
@@ -36,9 +52,13 @@ func NewFileLogger(filename string, level uint32) (*FileLogger, error) {
 
 // Init 初始化
 func (l *FileLogger) Init() error {
-	fp, err := os.OpenFile(l.Logfile, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
+	fileName := l.Logfile
+	if FILE_MODE_DAILY == l.Mode {
+		fileName = fileName + time.Now().Format("2006-01-02")
+	}
+	fp, err := os.OpenFile(fileName, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0666)
 	if nil != err {
-		return ErrOpenLogFileFail{l.Logfile, err}
+		return ErrOpenLogFileFail{fileName, err}
 	}
 
 	l.SetFormatter(&MixFormatter{})
@@ -74,8 +94,14 @@ func (l *FileLogger) Warning(msg string, data map[string]interface{}) {
 }
 
 // Error shortcut
-func (l *FileLogger) Error(msg string, data map[string]interface{}) {
-	l.WithFields(logrus.Fields(data)).Error(msg)
+// receive a error interface
+func (l *FileLogger) Error(msg interface{}, data map[string]interface{}) {
+	entry := l.WithFields(logrus.Fields(data))
+	if v, ok := msg.(string); ok {
+		entry.Error(v)
+	} else if v, ok := msg.(error); ok {
+		entry.Error(v.Error())
+	}
 }
 
 // Fatal shortcut
--- test/example.go
+++ test/example.go
@@ -1,20 +1,21 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/cyrnicolase/logger"
 )
 
 func main() {
-	log, _ := logger.NewFileLogger("./test.log", 5)
+	log, _ := logger.NewFileLogger("./test.log", 5, 0)
 
 	log.Debug("写一个Info日志", map[string]interface{}{
 		"foo": "bar",
 		"数字":  12345676,
 	})
 
-	log1, err := logger.NewFileLogger("/root/test1.log", 4)
+	log1, err := logger.NewFileLogger("/root/test1.log", 4, 1)
 
 	if nil != err {
 		fmt.Println(err)
@@ -24,4 +25,7 @@ func main() {
 	log1.Debug("写一个Debug 日志", map[string]interface{}{
 		"debug": "DEBUG",
 	})
+
+	// 测试Error接收error情况
+	log.Error(errors.New("写入一个错误接口对象"), nil)
 }
